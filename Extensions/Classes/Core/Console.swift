8005487584e431da62ae46f4611dc884b8bae8ca
xujx <jagni@live.com>
2019-05-01 18:07:06 +0800

    ex.po -> po

--- Extensions/Classes/Core/Console.swift
+++ Extensions/Classes/Core/Console.swift
@@ -1,5 +1,5 @@
 
-@available(*, deprecated, message: "Please use `ex.po` as a replacement.")
+@available(*, deprecated, message: "Please use `po` as a replacement.")
 public func debugLog<T>(_ message: T, file: String = #file, method: String = #function, line: Int = #line) {
     #if DEBUG
     print("-------\n",
@@ -7,5 +7,42 @@ public func debugLog<T>(_ message: T, file: String = #file, method: String = #fu
           "[\(line)],",
           "\(method):\n \(message)",
           "\n-------")
+    #endif
+}
+
+public enum POStyle: String {
+    case normal = "", warning = "⚠️", error = "❗️"
+}
+
+public func po(_ items: Any...,
+    id: String? = nil,
+    style: POStyle = .normal,
+    file: String = #file, method: String = #function, line: Int = #line) {
+    #if DEBUG
+    func p(_ items: Any..., terminator: String = " ") {
+        items.forEach {
+            print($0, terminator: terminator)
+        }
+    }
+    
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS:"
+    let dateString = dateFormatter.string(from: Date())
+    p(dateString)
+    
+    if let identifier = id {
+        p(identifier)
+    } else {
+        p("\((file as NSString).lastPathComponent):\(line).\(method)")
+    }
+    
+    if style != .normal {
+        p(style.rawValue)
+    }
+    
+    p("->")
+    items.forEach { p($0) }
+    print()
+    
     #endif
 }
--- Extensions/Classes/Core/Extensions.swift
+++ Extensions/Classes/Core/Extensions.swift
@@ -36,42 +36,6 @@ extension NSObject: ExtensionsCompatible { }
 
 public extension Extensions where Base: NSObject {
     
-    enum POStyle: String {
-        case normal = "", warning = "⚠️", error = "❗️"
-    }
-    
-    func po(_ items: Any...,
-        id: String? = nil,
-        style: POStyle = .normal,
-        file: String = #file, method: String = #function, line: Int = #line) {
-        #if DEBUG
-        func p(_ items: Any..., terminator: String = " ") {
-            items.forEach {
-                print($0, terminator: terminator)
-            }
-        }
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS:"
-        let dateString = dateFormatter.string(from: Date())
-        p(dateString)
-        
-        if let identifier = id {
-            p(identifier)
-        } else {
-            p("\((file as NSString).lastPathComponent):\(line).\(method)")
-        }
-        
-        if style != .normal {
-            p(style.rawValue)
-        }
-        
-        p("->")
-        items.forEach { p($0) }
-        print()
-        
-        #endif
-    }
 }
 
 public extension Extensions where Base: UIColor {
