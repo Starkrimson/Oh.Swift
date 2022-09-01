5259afb2f0b5fe07dfe67cfc69edf85eacde0271
Tsui <starkrimson@icloud.com>
2022-09-01 15:20:03 +0800

    register and dequeue reusableView

--- Oh.Swift/Classes/Core/Extensions/UITableView+Oh.swift
+++ Oh.Swift/Classes/Core/Extensions/UITableView+Oh.swift
@@ -7,10 +7,21 @@ public extension OhSwift where Base: UITableView {
         let nib = UINib(nibName: name, bundle: nil)
         base.register(nib, forCellReuseIdentifier: name)
     }
+    
+    func register<T: UITableViewHeaderFooterView>(nib aClass: T.Type) {
+        let name = String(describing: aClass)
+        let nib = UINib(nibName: name, bundle: nil)
+        base.register(nib, forHeaderFooterViewReuseIdentifier: name)
+    }
 
     func register<T: UITableViewCell>(_ aClass: T.Type) {
         let name = String(describing: aClass)
         base.register(aClass, forCellReuseIdentifier: name)
+    }
+    
+    func register<T: UITableViewHeaderFooterView>(_ aClass: T.Type) {
+        let name = String(describing: aClass)
+        base.register(aClass, forHeaderFooterViewReuseIdentifier: name)
     }
 
     func dequeue<T: UITableViewCell>(_ reusableCell: T.Type) -> T {
@@ -19,5 +30,13 @@ public extension OhSwift where Base: UITableView {
             fatalError("\(name) is not registered❗️")
         }
         return cell
+    }
+    
+    func dequeue<T: UITableViewHeaderFooterView>(_ reusableView: T.Type) -> T {
+        let name = String(describing: reusableView)
+        guard let view = base.dequeueReusableHeaderFooterView(withIdentifier: name) as? T else {
+            fatalError("\(name) is not registered❗️")
+        }
+        return view
     }
 }
